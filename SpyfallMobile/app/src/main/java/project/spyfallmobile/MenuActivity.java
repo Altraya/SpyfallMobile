d44fbc4f5b6d7802e468bc10981c9262acc9f654
Altraya <karakayn@darluok-server.com>
2017-03-29 13:36:18 +0200

    Update menu

--- SpyfallMobile/app/src/main/java/project/spyfallmobile/MenuActivity.java
+++ SpyfallMobile/app/src/main/java/project/spyfallmobile/MenuActivity.java
@@ -2,12 +2,35 @@ package project.spyfallmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button _buttonCreateServer;
+    private Button _buttonJoinServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        _buttonCreateServer = (Button) this.findViewById(R.id.buttonCreateServer);
+        _buttonJoinServer = (Button) this.findViewById(R.id.buttonJoinServer);
+
+        _buttonCreateServer.setOnClickListener(this);
+        _buttonJoinServer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == _buttonCreateServer)
+        {
+            //launch create server view
+        }
+        else if(v == _buttonJoinServer)
+        {
+            //launch join server view
+        }
+
     }
 }
